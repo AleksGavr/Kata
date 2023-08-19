c977014b22a827a0d6764e9170f35e676d2e7e4b
AlexeyN <aleksgavr@gmail.com>
2023-08-19 19:31:57 +1200

    Start

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Input:");
         String input = in.nextLine().replaceAll("\\s", "");
-        in.close();
+//        in.close();
 
 //  проверяю корректно лм введена арифметическая операция
         if (input.contains("+")) oper = "+";
         else if (input.contains("-")) oper = "-";
         else if (input.contains("*")) oper = "*";
         else if (input.contains("/")) oper = "/";
-        else System.out.println("ФМОУЗ-ДОО нет такой операции");
+        else System.out.println("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
 
 //  делаю массив из аргументов, где разделителем является арифметическая операция из допустимых
         String[] arr = input.split("[+\\-*/]");
@@ -32,57 +32,74 @@ public class Main {
         int[] arabic2 = {100, 90, 50, 40, 10, 9, 5, 4, 1};
 
 //  проверяю является ли первый аргумент арабским числом
+        boolean ar1 = false;
         int argA1 = 0;
         for (int i = 0; i < arabic.length; i++) {
             if (arabic[i].equals(arg1)) {
                 argA1 = (i + 1);
+                ar1 = true;
             }
         }
 
 /*  если первый аргумент арабское число, то второе по условию тоже должно быть арабским, поэтому
 не заморачиваюсь и сразу определяю его значение. Так как
-пока еще не на "ты" с ООП в java, чтобы не путаться буду делать по нубски-топорно - всё процедурно. Иначе
-не скоро смогу сдать работу в академию, пока всё изучу. Да и будет ли тогда уже смысл сдавать? :D
-Поэтому заранее прошу прощение за такое неоправданно огромное количество строк в программе. Что с лузера взять? ;-)
- */
+пока еще не на "ты" с ООП в java и с методами постоянно косячу, чтобы не путаться буду делать по нубски-топорно
+ - всё процедурно. Иначе не скоро смогу сдать работу в академию, пока всё изучу. Да и будет ли тогда уже смысл
+  сдавать? :D Поэтому заранее прошу прощение за такое неоправданно огромное количество строк в программе.
+  Что с лузера взять? ;-)*/
+
+        boolean ar2 = false;
         int argA2 = 0;
         for (int i = 0; i < arabic.length; i++) {
-            if (arabic[i].equals(arg1)) {
+            if (arabic[i].equals(arg2)) {
                 argA2 = (i + 1);
+                ar2 = true;
             }
         }
 
 //  Преобразую переменные из строки в число и произвожу итоговое вычисление
-        if (oper.equals("+")) System.out.println(argA1 + argA2);
-        if (oper.equals("-")) System.out.println(argA1 - argA2);
-        if (oper.equals("*")) System.out.println(argA1 * argA2);
-        if (oper.equals("/")) System.out.println(argA1 / argA2);
+        if((ar1 == true) && (ar2 == true)) {
+            if (oper.equals("+")) System.out.println(argA1 + argA2);
+            if (oper.equals("-")) System.out.println(argA1 - argA2);
+            if (oper.equals("*")) System.out.println(argA1 * argA2);
+            if (oper.equals("/")) System.out.println(argA1 / argA2);
+        }
+        else System.out.println("используются одновременно разные системы счисления");
 
 //  Если это выражение записано не арабскими числами, тоже самое делаем с с римскими.
 //  сначала определяем эквивалент арабскими и производим вычисление
+        ar1 = false;
         int argR1 = 0;
         for (int i = 0; i < rome.length; i++) {
             if (rome[i].equals(arg1)) {
                 argR1 = (i + 1);
+                ar1 = true;
             }
         }
 
+        ar2 = false;
         int argR2 = 0;
         for (int i = 0; i < arabic.length; i++) {
             if (rome[i].equals(arg2)) {
                 argR2 = (i + 1);
+                ar2 = true;
             }
         }
-//        System.out.println(argR2);
-        if (oper.equals("+")) System.out.println(argR1 + argR2);
-        if (oper.equals("-")) System.out.println(argR1 - argR2);
-        if (oper.equals("*")) System.out.println(argR1 * argR2);
-        if (oper.equals("/")) System.out.println(argR1 / argR2);
+        int num = 0;
+        if((ar1 == true) && (ar2 == true)) {
+            if (oper.equals("+")) num = (argR1 + argR2);
+            if (oper.equals("-")) num = (argR1 - argR2);
+            if (oper.equals("*")) num = (argR1 * argR2);
+            if (oper.equals("/")) num = (argR1 / argR2);
+        }
 
 //теперь преобразуем ответ обратно в римские числа
+//        String[] rome2 = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+//        int[] arabic2 = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+
         String result = "";
         int j = 0;
-        int num = 9;
+//        int num = 9;
 
         while (j < rome2.length) {
             while (num >= arabic2[j]) {
@@ -93,8 +110,8 @@ public class Main {
             }
             j++;
         }
-
-        //return ret.ToString();
+//        System.out.println(argR1);
+//        System.out.println(argR2);
         System.out.println(result);
 
     }
